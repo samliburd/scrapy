21a2a47a7544d114da4a9018c7f6733f704627b4
= <=>
2026-02-02 21:16:07 +0000

    query working

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import argparse
 import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument("url")
+parser.add_argument("url", help="The URL of the site")
+parser.add_argument("--read", help="Outputs the data", action="store_true")
 args = parser.parse_args()
 
 
@@ -26,16 +27,37 @@ def insert_data(table, url, date):
     return f"""INSERT INTO {table} VALUES(NULL, '{url}', '{date}');"""
 
 
+def output_data(table):
+    return f"""SELECT * FROM {table};"""
+
+
 def main():
-    connection = connect_to_db("urls.db")
-    cursor = connection.cursor()
+    print(args.read)
 
-    create_table(cursor)
-    insert_command = insert_data("urls", args.url, datetime.datetime.now())
-    cursor.execute(insert_command)
+    connection = None
+    try:
+        connection = connect_to_db("urls.db")
+        cursor = connection.cursor()
 
-    connection.commit()
-    connection.close()
+        create_table(cursor)
+        if args.read:
+            cursor.execute(output_data("urls"))
+            output = cursor.fetchall()
+            for url in output:
+                print(url)
+            return
+
+        insert_command = insert_data("urls", args.url, datetime.datetime.now())
+        cursor.execute(insert_command)
+
+        connection.commit()
+    except sqlite3.Error as e:
+        if connection:
+            connection.rollback()
+            print(f"A database error occurred: {e}")
+    finally:
+        if connection:
+            connection.close()
 
 
 if __name__ == "__main__":
